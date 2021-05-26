2e064c82200bf117c2cbf04c5eb3a310cff23858
QuimRobres <quim.robres@gmail.com>
2021-05-26 18:43:37 +0200

    -

--- src/components/General/Acordion/HomeAdminAccordion.js
+++ src/components/General/Acordion/HomeAdminAccordion.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './HomeAdminAccordion.css';
 import { Link } from 'react-router-dom';
 
+
 export default function Acordion() {
     return (
         <div className="activities-cards">
